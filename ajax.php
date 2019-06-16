de583edd2cf1ed80f1d561d2e9f44b17f0113d38
Thiago Alves <thiagoalves@albicod.com>
2019-06-16 09:59:53 -0300

    Adicionando Filtro de Meses

--- ajax.php
+++ ajax.php
@@ -1,132 +1,21 @@
 <?php
-    require 'autoload.php';
+require 'autoload.php';
 
-    //registrando lembrete em banco de dados
-    if (!empty($_POST['lembrete'])) {
-        $lembrete = addslashes($_POST['lembrete']);
-        $data = addslashes($_POST['data']);
+if (!empty($_POST['mes'])):
 
-        if (date('Y-m-d', strtotime($data)) >= date('Y-m-d')) {
-            $sql = (new Cadastro())->setAgenda($lembrete, $data);
-            echo '<script> alert("Cadastro realizado com sucesso!"); window.location.href = "index.php"; </script>';
-        } else {
-            echo '<script> alert("Cadastro não realizado - Abaixo da data atual!"); window.location.href = "index.php"; </script>';
-        }
-        
-    }
-
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Agenda</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
-    <script type="text/javascript" src="js/script.js"></script>
-
-    <style type="text/css">
-        #lembrete{
-            height: 250px;
-            background-color: #000;
-            color: #fff;
-            font-size: 20px;
-        }
-        .lembretes{
-            width: 100%;
-            height: 100%;
-            background-color: #B0C4DE;
-            font-size: 14px;
-            margin-top: 5px;
-            text-align: center;
-            word-wrap: break-word;
-        }
-        .lembretes:hover{
-            background-color: #4682B4;
-            color: #fff;
-        }
-        .th{
-            text-align: center;
-        }
-
-        .send {
-          box-shadow: 0 0 0 0 rgba(69, 152, 27, 0.5);
-        }
-
-        .send {
-          animation: pulse 1.5s infinite;
-        }
-
-        @keyframes pulse {
-          0% {
-            transform: scale(.9);
-          }
-          70% {
-            transform: scale(1);
-            box-shadow: 0 0 0 50px rgba(69, 152, 27, 0);
-          }
-          100% {
-            transform: scale(.9);
-            box-shadow: 0 0 0 0 rgba(69, 152, 27, 0);
-          }
-        }
-    </style>
-</head>
-<body>
-    <div class="container">
-        <?php
-        setlocale(LC_TIME, "pt_BR", "pt_BR.utf-8", "portuguese");
-        date_default_timezone_set('America/Sao_Paulo');
-
-        $data = date('Y-m');
-        if (utf8_decode(strftime("%B", strtotime($data))) == 'mar?o') {
-            echo '<h3 id="sel">'.strftime("Março de %Y", strtotime($data)).'</h3>';
-        } else {
-            echo '<h3 id="sel">'.ucfirst(strftime("%B de %Y", strtotime($data))).'</h3>';
-        }
-        ?>
-        <div class="row">
-            <div class="col-sm-6">
-                <h4>Agenda</h4>
-            </div>
-            <div class="col-sm-6">
-                <select class="form-control mes">
-                    <option>Selecione um mês</option>
-                    <?php
-                    $mesAtual = date('Y-m');
-                    for ($i=1; $i < 13; $i++) { 
-                        $dt = date('Y-'.$i);
-
-                        if ($mesAtual == $dt) {
-                            if (utf8_decode(strftime("%B", strtotime($dt))) == 'mar?o') {
-                                echo '<option selected value="'.$i.'">'.ucfirst(strftime("Março", strtotime($dt))).'</option>';
-                            } else {
-                                echo '<option selected value="'.$i.'">'.ucfirst(strftime("%B", strtotime($dt))).'</option>';
-                            }
-                        } else {
-                            if (utf8_decode(strftime("%B", strtotime($dt))) == 'mar?o') {
-                                echo '<option value="'.$i.'">'.ucfirst(strftime("Março", strtotime($dt))).'</option>';
-                            } else {
-                                echo '<option value="'.$i.'">'.ucfirst(strftime("%B", strtotime($dt))).'</option>';
-                            }
-                        }
-                        
-                    }
-                    ?>
-                </select>
-            </div>
-        </div>
-        <hr>
-
+	?>
         <table class="table table-hover" id="resultado">
             <tr>
             <?php
             $x = 0;
-            for ($i=1; $i <= date('t'); $i++) { 
+            $numero = cal_days_in_month(CAL_GREGORIAN, $_POST['mes'], date('Y'));
+
+            for ($i=1; $i <= $numero; $i++) { 
                 $x++;
 
                 $id = 'ag'.$i;
-                $v['data'] = date('Y-m-').$i;
+                $v['data'] = date('Y-'.$_POST['mes'].'-'.$i);
+
                 $lem = (new Cadastro())->getAgendasData($v['data']);
 
                 if (date('l', strtotime($v['data'])) == 'Monday') {
@@ -157,8 +46,8 @@
                     if (!empty($lem)) {
                         $data = date('Y-m-d', strtotime($v['data']));
 
-                        if ($data >= date('Y-m-d')) {
-                            if ($data == date('Y-m-d')) {
+                        if ($data >= date('Y-'.$_POST['mes'].'-d')) {
+                            if ($data == date('Y-'.$_POST['mes'].'-d')) {
                                 if ($dia == 'Sábado') {
                                     echo '<button title="'.$dia.'" data-toggle="modal" data-target="#'.$id.'" class="btn btn-success btn-block send">'.$i.'<br>'.substr($dia, 0, 4).'</button>';
                                 } else {
@@ -191,6 +80,7 @@
                     }
 
                     ?>
+                <!-- MODAL PARA LEMBRETE -->
                 <div id="<?=$id; ?>" class="modal fade" role="dialog">
                     <div class="modal-dialog">
                         <div class="modal-content">
@@ -198,7 +88,7 @@
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                                 <h4 class="modal-title">
                                     <?php
-                                    $dt = date('Y-m-').$i;
+                                    $dt = date('Y-'.$_POST['mes'].'-'.$i);
                                     $lembretes = (new Cadastro())->getAgendasData($dt);
                                     echo date('d/m/Y', strtotime($dt));
                                     ?>
@@ -208,7 +98,6 @@
                             <div class="modal-body">
                                 <p>
                                     <h3>Lembretes do dia</h3>
-
                                     <?php
                                     foreach ($lembretes as $l) {
                                        echo '<div class="lembretes">'.htmlspecialchars(substr($l['lembrete'], 0, 50)).' - Cadastrado em: '.date('d/m/Y H:i', strtotime($l['dt_cadastro'])).'</div>';
@@ -229,6 +118,7 @@
                         </div>
                     </div>
                 </div>
+    
                 <?php
 
                     echo '</td>';
@@ -239,8 +129,8 @@
                     if (!empty($lem)) {
                         $data = date('Y-m-d', strtotime($v['data']));
 
-                        if ($data >= date('Y-m-d')) {
-                            if ($data == date('Y-m-d')) {
+                        if ($data >= date('Y-'.$_POST['mes'].'m-d')) {
+                            if ($data == date('Y-'.$_POST['mes'].'-d')) {
                                 if ($dia == 'Sábado') {
                                     echo '<button title="'.$dia.'" data-toggle="modal" data-target="#'.$id.'" class="btn btn-success btn-block send">'.$i.'<br>'.substr($dia, 0, 4).'</button>';
                                 } else {
@@ -272,8 +162,8 @@
                         
                     }
 
-
                     ?>
+                <!-- MODAL PARA LEMBRETE -->
                 <div id="<?=$id; ?>" class="modal fade" role="dialog">
                     <div class="modal-dialog">
                         <div class="modal-content">
@@ -281,7 +171,7 @@
                                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                                 <h4 class="modal-title">
                                     <?php
-                                    $dt = date('Y-m-').$i;
+                                    $dt = date('Y-'.$_POST['mes'].'-'.$i);
                                     $lembretes = (new Cadastro())->getAgendasData($dt);
                                     echo date('d/m/Y', strtotime($dt));
                                     ?>
@@ -291,7 +181,6 @@
                             <div class="modal-body">
                                 <p>
                                     <h3>Lembretes do dia</h3>
-
                                     <?php
                                     foreach ($lembretes as $l) {
                                        echo '<div class="lembretes">'.htmlspecialchars(substr($l['lembrete'], 0, 50)).' - Cadastrado em: '.date('d/m/Y H:i', strtotime($l['dt_cadastro'])).'</div>';
@@ -312,21 +201,17 @@
                         </div>
                     </div>
                 </div>
+    
                 <?php
-
 
                     echo '</td>';
                     echo '</tr>';
                     //echo '<tr>';
                 }
-                
                 }
             ?>
             </tbody>
         </table>
-    </div>
-</body>
-</html>
+    <?php
 
-
-
+endif;
